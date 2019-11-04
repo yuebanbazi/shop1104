8f424af098c9b3e4add79711e1ed437a14c8749b
yuebanbazi <359454330@qq.com>
2019-11-04 18:11:25 +0800

    1104

---
+++ demo/src/main/java/com/example/demo/controller.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class controller {
+    @RequestMapping("hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
+}
